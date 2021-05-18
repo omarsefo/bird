a8ca2457b1b1e328c5835d9be18496798c1db913
unknown <omarsefo7@gmail.com>
2021-05-19 01:08:36 +0300

    update

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", spinner());
+function spinner(){
     setTimeout(function() {
         document.querySelector(".spinner").style.display = "none";
-    },2000)
+    },6000)
+}
+var btn =document.getElementById('btn');
+
+btn.addEventListener('click',spinner);
+
+window.addEventListener("scroll", function(){
+    var nav = document.querySelector(".navbar");
+nav.classList.toggle("sticky", window.scrollY > 15);
+document.body.style.scrollBehavior="smooth";
 });
 
